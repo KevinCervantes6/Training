1fd4e2ef398c9f61787b375c098cc85914abd2b7
KevinCervantesG <kevin.cervantes@ejamerica.com>
2021-04-05 12:28:16 -0500

    encryption assignment

---
+++ 2021-04-05/encryption.js
@@ -0,0 +1,14 @@
+"use strict";
+//Encription class
+var Encrypt = /** @class */ (function () {
+    function Encrypt(data) {
+        this.data = data;
+    }
+    Encrypt.prototype.encrypt = function (data) {
+        return '';
+    };
+    Encrypt.prototype.decrypt = function (data) {
+        return '';
+    };
+    return Encrypt;
+}());
