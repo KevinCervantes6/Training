e46e60c001c6cfa1be38a77f46440a3b1aa18a44
KevinCervantesG <kevin.cervantes@ejamerica.com>
2021-04-28 14:01:11 -0500

    Paper.tsx added and axios installed

--- front-rep/src/containers/HomePage/Inbox.tsx
+++ front-rep/src/containers/HomePage/Inbox.tsx
@@ -3,43 +3,52 @@ import { Theme, withStyles, StyleRules } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { Component } from 'react';
+import MyPaper from '../../UI/Paper';
+import axios from 'axios';
 
-
-
-const useStyles = (theme: Theme):StyleRules  => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(1),
-            height: theme.spacing(16),
-        },
-    },
-})
 
 interface IProps {
-    classes: any;
+    loading: boolean;
+    data: any;
+    error: any;
 }
 
-class Inbox extends Component<IProps> {
+class Inbox extends Component {
+
+    state = {loading: true, data: null, error: null};
+
+    render () { return <InboxView {...this.state} />}
+    
+}
+
+
+class InboxView extends Component<IProps> {
+
+    renderLoading() {
+        const dataJSX = <h3>Loading...</h3>
+        return dataJSX;
+    }
+
+    renderError() {
+        const dataJSX = <h3>Sorry, an error ocurred!</h3>
+        return dataJSX;
+    }
+
+    renderSuccess() {
+        const dataJSX = <MyPaper />
+        return dataJSX;
+    }
 
     render () {
-        return(
-            <div className={this.props.classes.root}>
-                <Paper elevation={3}>
-                    <Typography variant="subtitle2" gutterBottom>
-                        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                    </Typography>
-                    <Typography variant="subtitle1" gutterBottom>
-                        body1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec est vel elit tincidunt condimentum. 
-                        Sed vitae dolor quam. Donec efficitur porta ornare. Etiam luctus mauris nec diam scelerisque porttitor. 
-                        Integer eget semper massa. Duis id leo vehicula, vulputate felis quis, mattis mi. Sed bibendum tellus a eros ullamcorper dictum. 
-                    </Typography>
-                </Paper>
-            </div>
-        );
+        if(this.props.loading) {
+            return this.renderLoading();
+        } else if(this.props.data) {
+            return this.renderSuccess();
+        } else {
+            return this.renderError();
+        }
     }
     
 }
 
-export default withStyles(useStyles) (Inbox);
+export default Inbox;
